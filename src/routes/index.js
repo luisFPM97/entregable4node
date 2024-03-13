20b9aa0a23a0bf2e368e83b42ef16eff2fe2fbe1
luisFPM97 <134348310+luisFPM97@users.noreply.github.com>
2024-03-13 13:29:01 -0500

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const userRouter = require('./user.router');
-const emailRouter = require('./emailCode.router');
 const router = express.Router();
 
 // colocar las rutas aquí
